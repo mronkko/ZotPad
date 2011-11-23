d9885a4a1c70c7356121c5b0174db53d9f583387
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2011-11-23 13:32:57 +0200

    Added readme and a couple of missing files

---
+++ ZotPad/ZPZoteroItem.h
@@ -0,0 +1,13 @@
+//
+//  ZPZoteroItem.h
+//  ZotPad
+//
+//  Created by Rönkkö Mikko on 11/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZPZoteroItem : NSObject
+
+@end
---
+++ ZotPad/ZPZoteroItem.m
@@ -0,0 +1,13 @@
+//
+//  ZPZoteroItem.m
+//  ZotPad
+//
+//  Created by Rönkkö Mikko on 11/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ZPZoteroItem.h"
+
+@implementation ZPZoteroItem
+
+@end
