f5da9c8b50e1613b956d889f344ce0192f3ac1fd
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2011-12-01 10:28:19 +0200

    Work on the user interface and bug fixes. Added Three20UI, DSActivityView, and FMDB libraries. The repository should be cleaned up and these should be set up appropriately as dependencies.

--- ZotPad/ZPNavigatorNode.h
+++ ZotPad/ZPNavigatorNode.h
@@ -2,15 +2,12 @@
 //  ZPNavigatorNode.h
 //  ZotPad
 //
-//  Created by Rönkkö Mikko on 11/24/11.
+//  Created by Rönkkö Mikko on 11/30/11.
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @protocol ZPNavigatorNode <NSObject>
--(BOOL) hasChildren;
--(NSInteger) libraryID;
--(NSInteger) collectionID;
--(NSString*) name;
+
 @end
