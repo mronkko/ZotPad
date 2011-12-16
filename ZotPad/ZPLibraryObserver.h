b3a38a94c9369b5a0d656041832cb5aa0c9c9904
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2011-12-16 09:46:39 +0200

    New protocol to handle updating libraries and collections

---
+++ ZotPad/ZPLibraryObserver.h
@@ -0,0 +1,16 @@
+//
+//  ZPLibraryObserver.h
+//  ZotPad
+//
+//  Created by Rönkkö Mikko on 12/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZPZoteroLibrary.h"
+@protocol ZPLibraryObserver <NSObject>
+
+// Tells an observer that a library and all its collections are now available
+-(void) notifyLibraryWithCollectionsAvailable:(ZPZoteroLibrary*) library;
+
+@end
