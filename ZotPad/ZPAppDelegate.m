dd07e19db8428be752e86693cd826f1931ffe5b9
Mikko Rönkkö <mikko.ronkko@iki.fi>
2012-10-17 22:07:02 +0300

    iOS 6 compatibility

--- ZotPad/ZPAppDelegate.m
+++ ZotPad/ZPAppDelegate.m
@@ -117,16 +117,16 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
      //Manual override for userID and Key. Useful for running the code in debugger with other people's credentials.
     
     /*
-    
     NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:@"" forKey:@"userID"];
     [defaults setObject:@"" forKey:@"OAuthKey"];
 
+    
     //Uncomment these to always reset the app after launch
     [[ZPDatabase instance] resetDatabase];
     [[ZPCacheController instance] performSelectorInBackground:@selector(purgeAllAttachmentFilesFromCache) withObject:NULL];
-    
     */
+    
     
     [[ZPPreferences instance] checkAndProcessApplicationResetPreferences];
      
