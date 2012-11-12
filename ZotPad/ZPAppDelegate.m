94ff34e9a88829744610aa28939b1e5c99e3f026
Mikko Rönkkö <mikko.ronkko@iki.fi>
2012-11-12 09:36:53 +0200

    Logging to follow state

--- ZotPad/ZPAppDelegate.m
+++ ZotPad/ZPAppDelegate.m
@@ -167,6 +167,7 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    DDLogVerbose(@"Will resign active");
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -175,6 +176,9 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+
+    DDLogVerbose(@"Did enter background");
+
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
@@ -183,6 +187,8 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    DDLogVerbose(@"Will enter foreground");
+
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
@@ -193,6 +199,9 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+
+    DDLogVerbose(@"Did become active");
+
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
@@ -205,7 +214,7 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
      Save data if appropriate.
      See also applicationDidEnterBackground:.
      */
-    DDLogInfo(@"Terminating");
+    DDLogInfo(@"Will terminate");
 
     
 }
