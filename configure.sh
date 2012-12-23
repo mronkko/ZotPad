a3430b409638a4ad222d4df5f3a3ea72aea242c8
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2012-12-23 13:04:11 +0200

    Working on configure scripts and documentation

---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#  configure.sh
+#  ZotPad
+#
+#  Created by Mikko Rönkkö on 12/23/12.
+#
+
+git pull FMDB
+
+git pull OAuthConsumer
+
+git pull iCarousel
+
+git pull json-framework
+
+git pull asi-http-request
+
+git pull Oxygen-Icons
+
+git pull OHAttributedLabel
+
+git pull DTCoreText
+
+git pull FileMD5Hash
+
+git pull CocoaLumberjack
+
+git pull InAppSettingsKit
+
+git pull Base64
+
+git pull dropbox-sdk-ios
+
+git pull FRLayeredNavigationController
+
+git pull CoconutKit
+
+git pull CSLProcessor
+
+git pull uservoice-iphone-sdk
+
+git pull CMPopTipView
