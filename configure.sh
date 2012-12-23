10203fe15eadc1aea73f8bc15b2b1aa9ddc517de
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2012-12-23 13:15:16 +0200

    Working on the configure script

--- configure.sh
+++ configure.sh
@@ -8,38 +8,4 @@
 
 git submodule init
 
-git pull FMDB
-
-git pull OAuthConsumer
-
-git pull iCarousel
-
-git pull json-framework
-
-git pull asi-http-request
-
-git pull Oxygen-Icons
-
-git pull OHAttributedLabel
-
-git pull DTCoreText
-
-git pull FileMD5Hash
-
-git pull CocoaLumberjack
-
-git pull InAppSettingsKit
-
-git pull Base64
-
-git pull dropbox-sdk-ios
-
-git pull FRLayeredNavigationController
-
-git pull CoconutKit
-
-git pull CSLProcessor
-
-git pull uservoice-iphone-sdk
-
-git pull CMPopTipView
+git submodule foreach git pull
