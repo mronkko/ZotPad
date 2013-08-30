a1f71f4e9d20cae11efce53e2e8d92b16e26a4d1
Mikko Rönkkö <mikko.ronkko@aalto.fi>
2013-08-30 21:16:01 +0300

    Fixes an iOS 7 bug
    https://devforums.apple.com/thread/197966?start=50&tstart=0

--- ZotPad/main.m
+++ ZotPad/main.m
@@ -10,8 +10,21 @@
 
 #import "ZPAppDelegate.h"
 
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+        return 0;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+
 int main(int argc, char *argv[])
 {
+    _oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZPAppDelegate class]));
     }
